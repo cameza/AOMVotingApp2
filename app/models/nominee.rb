ff4227cf21bf656207ed871988fb8961bfa128c0
cameza <camilo.meza@me.com>
2015-03-09 00:54:16 +0000

    Title error fixed, validations added and formatted for bootstrap

--- app/models/nominee.rb
+++ app/models/nominee.rb
@@ -1,2 +1,5 @@
 class Nominee < ActiveRecord::Base
+	validates :mail, :name, :cv, :statement, presence: true
+	validates_format_of :mail, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "doesn't look like an email address" 
+	validates :mail, :uniqueness => { :message => " Warning! - That email belongs to a fellow that was already nominated" }
 end
